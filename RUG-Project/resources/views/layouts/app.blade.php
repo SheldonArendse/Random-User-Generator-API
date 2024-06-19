66e0c065d99d41990caebafafde4589f61fa8dfc
SheldonArendse <126950809+SheldonArendse@users.noreply.github.com>
2024-06-19 20:17:13 +0200

    Added basic table css

--- RUG-Project/resources/views/layouts/app.blade.php
+++ RUG-Project/resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel App</title>
+    <title>Random User Generator</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
