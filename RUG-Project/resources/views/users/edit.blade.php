51f32d7158e2b53ffa2b0486c18cfca7e576c3e0
SheldonArendse <126950809+SheldonArendse@users.noreply.github.com>
2024-06-20 14:46:57 +0200

    more changes

--- RUG-Project/resources/views/users/edit.blade.php
+++ RUG-Project/resources/views/users/edit.blade.php
@@ -1,52 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Edit User</h1>
+    <div class="edit-form">
+        <h1 id="edit-user-title">Edit User</h1>
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-    <form action="{{ route('users.update', $user->id) }}" method="POST">
-        @csrf
-        @method('PUT')
+        <form action="{{ route('users.update', $user->id) }}" method="POST">
+            @csrf
+            @method('PUT')
 
-        <div class="form-group">
-            <label for="title">Title</label>
-            <select name="title" class="form-control" required>
-                <option value="Mr" {{ $user->title == 'Mr' ? 'selected' : '' }}>Mr</option>
-                <option value="Ms" {{ $user->title == 'Ms' ? 'selected' : '' }}>Ms</option>
-                <option value="Mrs" {{ $user->title == 'Mrs' ? 'selected' : '' }}>Mrs</option>
-                <option value="Dr" {{ $user->title == 'Dr' ? 'selected' : '' }}>Dr</option>
-            </select>
-        </div>
+            <div class="form-group">
+                <label for="title" id="label-style-1">Title</label>
+                <select name="title" class="form-control" id="title-cbx" required>
+                    <option value="Mr" {{ $user->title == 'Mr' ? 'selected' : '' }}>Mr</option>
+                    <option value="Ms" {{ $user->title == 'Ms' ? 'selected' : '' }}>Ms</option>
+                    <option value="Mrs" {{ $user->title == 'Mrs' ? 'selected' : '' }}>Mrs</option>
+                    <option value="Dr" {{ $user->title == 'Dr' ? 'selected' : '' }}>Dr</option>
+                </select>
+            </div>
 
-        <div class="form-group">
-            <label for="name">First Name</label>
-            <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $user->name) }}" required>
-        </div>
+            <div class="form-group" id="form-group-edit">
+                <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $user->name) }}" required>
+                <label for="name" id="label-style-1">First Name:</label>
+            </div>
 
-        <div class="form-group">
-            <label for="surname">Surname</label>
-            <input type="text" name="surname" id="surname" class="form-control" value="{{ old('surname', $user->surname) }}" required>
-        </div>
+            <div class="form-group" id="form-group-edit">
+                <input type="text" name="surname" id="surname" class="form-control" value="{{ old('surname', $user->surname) }}" required>
+                <label for="surname" id="label-style-1">Surname:</label>
+            </div>
 
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" class="form-control" value="{{ old('email', $user->email) }}" required>
-        </div>
+            <div class="form-group" id="form-group-edit">
+                <input type="email" name="email" id="email" class="form-control" value="{{ old('email', $user->email) }}" required>
+                <label for="email" id="label-style-1">Email:</label>
+            </div>
 
-        <div class="form-group">
-            <label for="phone">Phone</label>
-            <input type="text" name="phone" id="phone" class="form-control" value="{{ old('phone', $user->phone) }}" required>
-        </div>
+            <div class="form-group" id="form-group-edit">
+                <input type="text" name="phone" id="phone" class="form-control" value="{{ old('phone', $user->phone) }}" required>
+                <label for="phone" id="label-style-1">Phone:</label>
+            </div>
 
-        <button type="submit" class="btn btn-primary">Update</button>
-    </form>
+            <button type="submit" class="btn-style-1">Update</button>
+        </form>
+    </div>
 @endsection
